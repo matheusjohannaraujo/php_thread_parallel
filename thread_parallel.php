622141242114e0c9dfd88a4dbb4811dc12d3f3c4
matheusjohannaraujo <matheusjohannaraujo@gmail.com>
2020-12-29 12:00:25 -0300

    Fix - Closing connections that were open

--- thread_parallel.php
+++ thread_parallel.php
@@ -5,7 +5,7 @@
 	Country: Brasil
 	State: Pernambuco
 	Developer: Matheus Johann Araujo
-	Date: 2020-12-26
+	Date: 2020-12-29
 */
 
 error_reporting(E_ALL);
@@ -25,8 +25,19 @@ ini_set("max_file_uploads", 200);
 - https://www.toni-develops.com/2017/09/05/curl-multi-fetch/
 - https://imasters.com.br/back-end/non-blocking-asynchronous-requests-usando-curlmulti-e-php */
 
-function thread_parallel(array $script, string $thread_http = "http://localhost/thread_parallel/thread_http.php") :array
+/**
+ * @param string|array $script
+ * @param string $thread_http [optional, default = "http://localhost/php_thread_parallel/thread_http.php"]
+ * @return Promise
+ */
+function thread_parallel($script, string $thread_http = "http://localhost/php_thread_parallel/thread_http.php") :array
 {
+    if (is_string($script)) {
+        $script = [$script];
+    }
+    if (!is_array($script)) {
+        $script = ['echo "invalid script";'];
+    }
     // Inicializa um multi-curl handle
     $mch = curl_multi_init();
     foreach ($script as $key => $value) {
@@ -41,8 +52,8 @@ function thread_parallel(array $script, string $thread_http = "http://localhost/
         unset($data);
     }    
     // Fica em busy-waiting até que todas as requisições retornem
-    $active = null;
     do {
+        $active = null;
         // Executa as requisições definidas no multi-curl handle, e retorna imediatamente o status das requisições
         curl_multi_exec($mch, $active);
         usleep(1);        
@@ -50,6 +61,12 @@ function thread_parallel(array $script, string $thread_http = "http://localhost/
     foreach ($script as $key => $ch) {
         // Acessa a resposta de cada requisição
         $script[$key] = base64_decode(curl_multi_getcontent($ch));
+        // Remove o channel ($ch) da requisição do multi-curl handle ($mch)
+        curl_multi_remove_handle($mch, $ch);
+        // Fecha o channel ($ch)
+        curl_close($ch);
     }
+    // Fecha o multi-curl handle ($mch)
+    curl_multi_close($mch);
     return $script;
 }
