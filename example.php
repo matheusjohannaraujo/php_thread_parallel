33d01083f7e91fbafbbf6158c683674d4cb5b3bf
matheusjohannaraujo <matheusjohannaraujo@gmail.com>
2020-12-29 16:29:03 -0300

    Request that does not wait for response

--- example.php
+++ example.php
@@ -5,7 +5,7 @@
 	Country: Brasil
 	State: Pernambuco
 	Developer: Matheus Johann Araujo
-	Date: 2020-12-26
+	Date: 2020-12-30
 */
 
 require_once "thread_parallel.php";
@@ -13,16 +13,18 @@ require_once "thread_parallel.php";
 /*
 
 	Este código permite executar scritps PHP de forma simultanea,
-	onde cada script será executado em uma Thread distinta.
+	onde cada script será executado em uma Thread distinta
 	-
 	Observação: Os scripts serão executados de forma paralela, porém
 	o script (main) que criou as Threads vai aguardar até que todos
 	os scripts terminem de executar para retornar o resultado da
 	execução de cada script. É semelhante a criar várias Threads e
 	executar o comando "JOIN" que só deixa o script principal (main)
-	prosseguir quando as Threads terminam de serem executadas.
+	prosseguir quando as Threads terminam de serem executadas
 
 */
+
+echo "<pre>";
 
 // Scripts PHP que serão executados na Thread HTTP
 $scripts = [
@@ -50,8 +52,30 @@ $scripts = [
 // Localização do arquivo Thread HTTP
 $thread_http = "http://localhost/php_thread_parallel/thread_http.php";
 
-// Cria uma thread para cada script, e aguarda o fim da execução de todos os scripts para obter o respectivo resultado de cada um.
+// Cria uma thread para cada script, e aguarda o fim da execução de todos os scripts para obter o respectivo resultado de cada um
 $scripts = thread_parallel($scripts, $thread_http);
 
-// Mostra o conteúdo retornado ao fim da execução de cada script.
+// Mostra o conteúdo retornado ao fim da execução de cada script
+var_export($scripts);
+
+echo "<hr>";
+
+// ------------------------------------------------
+// Executa script sem esperar pelo retorno
+
+$scripts = thread_parallel(
+'
+	
+	file_put_contents("file.txt", "inicio\r\n");
+	for ($i = 1; $i <= 20; $i++) { 
+		sleep(1);
+		file_put_contents("file.txt", "$i\r\n");
+	}
+	file_put_contents("file.txt", "fim\r\n");
+	sleep(1);
+	unlink("file.txt");
+	
+', $thread_http, false);
+
+// Mostra o status da requisição de cada script
 var_export($scripts);
--- thread_parallel.php
+++ thread_parallel.php
@@ -5,7 +5,7 @@
 	Country: Brasil
 	State: Pernambuco
 	Developer: Matheus Johann Araujo
-	Date: 2020-12-29
+	Date: 2020-12-30
 */
 
 error_reporting(E_ALL);
@@ -25,12 +25,21 @@ ini_set("max_file_uploads", 200);
 - https://www.toni-develops.com/2017/09/05/curl-multi-fetch/
 - https://imasters.com.br/back-end/non-blocking-asynchronous-requests-usando-curlmulti-e-php */
 
+
 /**
+ *
  * @param string|array $script
  * @param string $thread_http [optional, default = "http://localhost/php_thread_parallel/thread_http.php"]
- * @return Promise
+ * @param bool $waitResponse [optional, default = true]
+ * @param bool $infoRequest [optional, default = false]
+ * @return array
  */
-function thread_parallel($script, string $thread_http = "http://localhost/php_thread_parallel/thread_http.php") :array
+function thread_parallel(
+    $script,
+    string $thread_http = "http://localhost/php_thread_parallel/thread_http.php",
+    bool $waitResponse = true,
+    bool $infoRequest = false
+) :array
 {
     if (is_string($script)) {
         $script = [$script];
@@ -38,35 +47,60 @@ function thread_parallel($script, string $thread_http = "http://localhost/php_th
     if (!is_array($script)) {
         $script = ['echo "invalid script";'];
     }
-    // Inicializa um multi-curl handle
-    $mch = curl_multi_init();
-    foreach ($script as $key => $value) {
-        $data = ["script" =>  base64_encode(trim($value))];
-        $script[$key] = null;
-        // Inicializa e seta as opções para cada requisição
-        $script[$key] = curl_init($thread_http);
-        curl_setopt($script[$key], CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($script[$key], CURLOPT_POSTFIELDS, $data);
-        // Adiciona a requisição channel ($script[$key]) ao multi-curl handle ($mch)
-        curl_multi_add_handle($mch, $script[$key]);
-        unset($data);
+    if (!$waitResponse) {
+        // https://www.php.net/manual/pt_BR/function.curl-setopt.php
+        // https://thiagosantos.com/blog/623/php/php-curl-timeout-e-connecttimeout/
+        foreach ($script as $key => $value) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $thread_http);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, ["script" => base64_encode(trim($value))]);
+            // Tempo em que o client pode aguardar para conectar no server
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+            // Tempo em que o solicitante espera por uma resposta
+            curl_setopt($ch, CURLOPT_TIMEOUT_MS, 500);
+            curl_exec($ch);
+            $script[$key] = [                
+                'response' => null,
+                "error" => curl_errno($ch) ? curl_error($ch) : null,
+                "info" => $infoRequest ? curl_getinfo($ch) : null
+            ];
+            curl_close($ch);            
+        }
+    } else {
+        // Inicializa um multi-curl handle
+        $mch = curl_multi_init();
+        foreach ($script as $key => $value) {
+            $script[$key] = null;
+            // Inicializa e seta as opções para cada requisição
+            $script[$key] = curl_init();
+            curl_setopt($script[$key], CURLOPT_URL, $thread_http);
+            curl_setopt($script[$key], CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($script[$key], CURLOPT_POSTFIELDS, ["script" => base64_encode(trim($value))]);
+            // Adiciona a requisição channel ($script[$key]) ao multi-curl handle ($mch)
+            curl_multi_add_handle($mch, $script[$key]);
+        }    
+        // Fica em busy-waiting até que todas as requisições retornem
+        do {
+            $active = null;
+            // Executa as requisições definidas no multi-curl handle, e retorna imediatamente o status das requisições
+            curl_multi_exec($mch, $active);
+            usleep(50);        
+        } while($active > 0);
+        foreach ($script as $key => $ch) {
+            $script[$key] = [            
+                "response" => base64_decode(curl_multi_getcontent($ch)),// Acessa a resposta de cada requisição
+                "error" => curl_errno($ch) ? curl_error($ch) : null,
+                "info" => $infoRequest ? curl_getinfo($ch) : null
+            ];
+            // Remove o channel ($ch) da requisição do multi-curl handle ($mch)
+            curl_multi_remove_handle($mch, $ch);
+            // Fecha o channel ($ch)
+            curl_close($ch);
+        }
+        // Fecha o multi-curl handle ($mch)
+        curl_multi_close($mch);
     }    
-    // Fica em busy-waiting até que todas as requisições retornem
-    do {
-        $active = null;
-        // Executa as requisições definidas no multi-curl handle, e retorna imediatamente o status das requisições
-        curl_multi_exec($mch, $active);
-        usleep(1);        
-    } while($active > 0);
-    foreach ($script as $key => $ch) {
-        // Acessa a resposta de cada requisição
-        $script[$key] = base64_decode(curl_multi_getcontent($ch));
-        // Remove o channel ($ch) da requisição do multi-curl handle ($mch)
-        curl_multi_remove_handle($mch, $ch);
-        // Fecha o channel ($ch)
-        curl_close($ch);
-    }
-    // Fecha o multi-curl handle ($mch)
-    curl_multi_close($mch);
     return $script;
 }
